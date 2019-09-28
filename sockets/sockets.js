583359432a27d36fe9f497af63b932801003d124
Anthony Cudzinovic <cudzinovica@gmail.com>
2019-09-27 20:02:34 -0600

    ability to leave game

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,17 +1,26 @@
 var GameActionsService = require('../services/gameActions.service');
 var GamesService = require('../services/games.service');
 
+function getAndBroadcastGame(io, gameId) {
+    GamesService.getGame(gameId, true).then(game => {
+        io.to(gameId).emit('game', game);
+    });
+}
+
 module.exports = function(io) {
     io.on('connection', socket => {
-        let gameId;
         socket.on('join-game', data => {
-            gameId = data.gameId;
+            const gameId = data.gameId;
             socket.join(gameId);
 
-            GamesService.getGame(gameId).then(game => {
-                io.to(gameId).emit('game', game);
-            });
+            getAndBroadcastGame(io, gameId);
         });
+        socket.on('leave-game', data => {
+            const gameId = data.gameId;
+            socket.leave(gameId);
+
+            getAndBroadcastGame(io, gameId);
+        })
         socket.on('start-game', data => {
             const gameId = data.gameId;
 
@@ -24,6 +33,7 @@ module.exports = function(io) {
                 //emit error to socket
                 socket.emit('error', resp);
             }
-        })
+        });
+
     });
 }
