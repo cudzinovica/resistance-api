2edc3c45739d0e93ae5b2942a6ea68a9f29346db
Anthony Cudzinovic <cudzinovica@gmail.com>
2019-10-07 11:52:56 -0600

    add disconnect print statement

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -9,18 +9,29 @@ function getAndBroadcastGame(io, gameId) {
 
 module.exports = function(io) {
     io.on('connection', socket => {
+        socket.on('disconnect', _ => {
+            console.log('**DISCONNECTED**');
+        });
+
+        /** Set socket's player id. Join socket to game room. Broadcast game to room. Emit player id to socket.*/
         socket.on('join-game', data => {
             const gameId = data.gameId;
+
             socket.join(gameId);
 
             getAndBroadcastGame(io, gameId);
         });
+
+        /** Delete player. Socket leaves game room. Broadcast game to room. */
         socket.on('leave-game', data => {
             const gameId = data.gameId;
+
             socket.leave(gameId);
 
             getAndBroadcastGame(io, gameId);
-        })
+        });
+
+        /** Start game. Broadcast game to room. */
         socket.on('start-game', data => {
             const gameId = data.gameId;
 
@@ -41,6 +52,5 @@ module.exports = function(io) {
                 socket.emit('error_msg', error);
             });
         });
-
     });
 }
